612ce62daaa9038fb9f77125f2143c6dbd839570
kdaud <dkakumirizii@gmail.com>
2020-12-21 10:23:08 +0300

    i/o api

--- src/com/jabrefcontent/dao/FileReader.java
+++ src/com/jabrefcontent/dao/FileReader.java
@@ -1,6 +1,33 @@
 package com.jabrefcontent.dao;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class FileReader {
+	
+	File file = new File("/home/opensource/Desktop/kdaud.txt");
+	
+
+	//String string = "";
+	
+	
+
+	public void myfilereader() throws IOException {
+		Scanner scanner=new Scanner(file);
+		System.out.println(scanner.nextLine());
+		System.out.println("************************************");
+		
+		String filecontent="";
+		while (scanner.hasNextLine()) {
+			filecontent = filecontent.concat(scanner.nextLine() + "\n");
+			//System.out.println(scanner.nextLine());
+			
+		}
+		FileWriter writer = new FileWriter("/home/opensource/Desktop/software.txt");
+		writer.write(filecontent);
+		writer.close();
+	}
 	
 }
---
+++ src/com/jabrefcontent/dao/TesterClass.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.jabrefcontent.dao;
+
+import java.io.IOException;
+
+/**
+ * @author opensource
+ *
+ */
+public class TesterClass {
+	
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		new FileReader().myfilereader();
+		
+	}
+	
+}
